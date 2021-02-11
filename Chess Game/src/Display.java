8129ee831d7c8697ff6a11333727736c3dc6bef5
TommyDeNezza <thomasdenezza@gmail.com>
2021-02-11 08:51:35 -0700

    Added modified board and began listeners

--- Chess Game/src/Display.java
+++ Chess Game/src/Display.java
@@ -3,46 +3,44 @@ import javax.swing.*;
 public class Display extends Canvas
 	{
 	private static final long serialVersionUID = 1L;
+	private static Color darkSQ = new Color(255, 248, 200);
 	public static void displayWindow()
 		{
 		Display canvas = new Display();
-		JFrame frame = new JFrame("ChessFrame");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(900, 900);
-		frame.getContentPane().add(canvas).setBackground(Color.white);
+		JFrame frame = new JFrame("Tony Hawk's Chess");
+		frame.getContentPane().addMouseListener(new ChessClickListener());
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.setSize(826, 849);
+		frame.getContentPane().add(canvas).setBackground(darkSQ);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		}
 	public void paint(Graphics graphics)
 		{
-		for(int i = 33; i < 800; i += 200)
+		for(int i = 5; i < 800; i += 200)
 			{
-			for(int j = 728; j > 0; j-=200)
+			for(int j = 5; j < 800; j += 200)
 				{
-				graphics.setColor(Color.black);
+				graphics.setColor(Color.DARK_GRAY);
 				graphics.fillRect(i, j, 100, 100);
 				}
 			}
-		for(int i = 133; i < 800; i += 200)
+		for(int i = 105; i < 805; i += 200)
 			{
-			for(int j = 628; j > 0; j-=200)
+			for(int j = 105; j < 805; j += 200)
 				{
-				graphics.setColor(Color.black);
+				graphics.setColor(Color.DARK_GRAY);
 				graphics.fillRect(i, j, 100, 100);
 				}
 			}
 		graphics.setColor(Color.gray);
-		for(int i = 28; i < 34; i++)
+		for(int i = 0; i < 5; i++)
 			{
-			for(int j = 23; j < 29; j++)
-				{
-				graphics.drawLine(i, 828, i, 28);
-				//graphics.drawLine(33, 828, 833, 828);
-				//graphics.drawLine(833, 828, 833, 28);
-				//graphics.drawLine(33, 28, 833, 28);
-				}
+			graphics.drawLine(i, 810, i, 0);
+			graphics.drawLine(0, i, 810, i);
+			graphics.drawLine(805+i, 810, 805+i, 0);
+			graphics.drawLine(0, 805+i, 810, 805+i);
 			}
-        
 		}
 	}
